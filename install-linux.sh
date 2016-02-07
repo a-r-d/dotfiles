5f140d2f20766c8207c3ba8e735d8629284f8fb6
Aaron Decker <aaron@ard.ninja>
2016-02-07 16:32:48 -0500

    add jshint

--- install-linux.sh
+++ install-linux.sh
@@ -17,3 +17,6 @@ cd ~/.vim_runtime && python update_plugins.py && cd ..
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 cp ~/dotfiles/nix/.vim_runtime/my_configs.vim ~/.vim_runtime/my_configs.vim
 
+## things needed for plugins:
+npm install -g jshint
+
